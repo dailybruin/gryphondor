a2fa67a852b2a87b706cc03842953383bdc59411
Amy Tsao <amy.tsao227@gmail.com>
2014-05-31 09:51:55 -0700

    added map code

--- pages/page-interactive-crime-map-2014.php
+++ pages/page-interactive-crime-map-2014.php
@@ -6,4 +6,6 @@ Template Name: Interactive Crime Map 2014
 <style type="text/css">
 </style>
 
+<p><iframe src="//batchgeo.com/map/7fe49e50965f8c2faaf2135fce68e35c" frameborder="0" width="100%" height="550" style="border:1px solid #aaa;border-radius:10px;"></iframe></p><p><small>View <a href="http://batchgeo.com/map/7fe49e50965f8c2faaf2135fce68e35c">Crimewatch</a> in a full screen map</small></p>
+
 <?php get_footer(); ?>
